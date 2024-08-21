1f4127471bc406486c3fb9793940a7fd0f907799
i0Ek3 <kno30826@gmail.com>
2024-08-21 13:25:37 +0800

    Update README.md

--- download.sh
+++ download.sh
@@ -51,21 +51,9 @@ function download {
         "https://www.educity.cn/rk/zt/201811fenshu//zhentipdf/jiagoulunwen.pdf"\
         "https://www.educity.cn/rk/zt/201811fenshu/zhentipdf/jiagou2.pdf"\
         "https://www.educity.cn/rk/zt/201811fenshu/zhentipdf/jiagou1.pdf"\
-
-        # 2019
-        #"https://www.educity.cn/rk/zt/201911fenshu//zhentipdf/jiagoulunwen.pdf"\
-        #"https://www.educity.cn/rk/zt/201911fenshu/zhentipdf/jiagou2.pdf"\
-        #"https://www.educity.cn/rk/zt/201911fenshu/zhentipdf/jiagou1.pdf"\
-
-        # 2020
-        #"https://www.educity.cn/rk/zt/202011fenshu//zhentipdf/jiagoulunwen.pdf"\
-        #"https://www.educity.cn/rk/zt/202011fenshu/zhentipdf/jiagou2.pdf"\
-        #"https://www.educity.cn/rk/zt/202011fenshu/zhentipdf/jiagou1.pdf"\
     )
 
-    names=("09论文" "09案例" "09综合" "10论文" "10案例" "10综合" "11论文" "11案例" "11综合" "12论文" "12案例" "12综合" "13论文" "13案例" "13综合" "14论文" "14案例" "14综合" "15论文" "15案例" "15综合" "16论文" "16案例" "16综合" "17论文" "17案例" "17综合" "18论文" "18案例" "18综合"
-    # "19c" "19b" "19a" "20论文" "20案例" "20综合"
-    )
+    names=("09论文" "09案例" "09综合" "10论文" "10案例" "10综合" "11论文" "11案例" "11综合" "12论文" "12案例" "12综合" "13论文" "13案例" "13综合" "14论文" "14案例" "14综合" "15论文" "15案例" "15综合" "16论文" "16案例" "16综合" "17论文" "17案例" "17综合" "18论文" "18案例" "18综合")
 
     mkdir exam_09-18 ; cd exam_09-18
 
